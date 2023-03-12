d8aa8dd1eae1a04d7b2032d2c0939c21131a213b
Alatheesh <100457240+Alatheesh@users.noreply.github.com>
2023-03-12 19:45:25 +0530

    Update lyrics.py

--- plugins/ExraMods/lyrics.py
+++ plugins/ExraMods/lyrics.py
@@ -20,9 +20,9 @@ async def sng(bot, message):
           await mee.delete()
           try:
             await mee.delete()
-            await bot.send_message(chat_id, text = rpl, reply_to_message_id = message.id, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs ", url = f"t.me/mkn_bots_updates")]]))
+            await bot.send_message(chat_id, text = rpl, reply_to_message_id = message.id, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("𝗡𝗧𝗠", url = f"t.me/llathu63035")]]))
           except Exception as e:                            
-             await message.reply_text(f"I Can't Find A Song With `{song}`", quote = True, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url = f"t.me/mkn_bots_updates")]]))
+             await message.reply_text(f"I Can't Find A Song With `{song}`", quote = True, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("𝗡𝗧𝗠", url = f"t.me/llathu63035")]]))
 
 
 def search(song):
