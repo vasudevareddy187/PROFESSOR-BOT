72c5fd9426826bfb2280c2adc1b88d1161604778
vasudevareddy187 <127773975+vasudevareddy187@users.noreply.github.com>
2023-04-11 01:33:23 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -48,7 +48,7 @@ async def start(client, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_chat_action(enums.ChatAction.TYPING)
-        m=await message.reply_sticker("CAACAgEAAxkBAANnZCSNwRDJOCQPrwvoBpFX9jLPVWUAAjECAAKCoCBEBwswVayxxoceBA")
+        m=await message.reply_sticker("CAACAgUAAxkBAAEBC-lkNGtcaC7H8ZS59bJFavy4iJBVAgACqAADyJRkFJWi9VCRb0zWHgQ")
         await asyncio.sleep(1)
         await m.delete()
         await message.reply_photo(
